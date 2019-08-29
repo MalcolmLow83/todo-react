558d0c30936a2eccc1640beefb6e1d2807155c77
MalcolmLow83 <malcomlow@gmail.com>
2019-08-29 14:05:08 +0800

    sucessfully implemented the add item feature

--- script.jsx
+++ script.jsx
@@ -9,21 +9,33 @@ class List extends React.Component {
   }
 
   addItem(){
-    debugger;
+    this.state.list.push(this.state.word);
+    console.log("wordArraya: "+this.state.word);
+    let currentList = this.state.list;
+    this.setState({list:currentList});
+    console.log("wordArrayb: "+this.state.list);
   }
 
   changeHandler(){
-    debugger;
+    console.log("Typed: "+event.target.value); //broswer typed value
+    let currentWord = event.target.value; //pass the typed value into currentWord
+    this.setState({word:currentWord}); //set state on the word array
   }
 
   render() {
       // render the list with a map() here
+      let itemsElements = this.state.list.map((item,index) => {
+        return <li key={index}>{item}</li>
+      });
 
       console.log("rendering");
       return (
         <div className="list">
-          <input onChange={()=>{this.changeHandler()}} value={this.state.word}/>
-          <button onClick={()=>{this.addItem()}}>add item</button>
+            <input onChange={()=>{this.changeHandler()}} value={this.state.word}/>
+            <button onClick={()=>{this.addItem()}}>add item</button>
+            <ul>
+                {itemsElements}
+            </ul>
         </div>
       );
   }
@@ -33,4 +45,3 @@ ReactDOM.render(
     <List/>,
     document.getElementById('root')
 );
-
