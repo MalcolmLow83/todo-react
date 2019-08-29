9e56976587a634ff34aab011d29a73c2470a5805
MalcolmLow83 <malcomlow@gmail.com>
2019-08-29 18:47:08 +0800

    implemented moments.js feature

--- script.jsx
+++ script.jsx
@@ -1,7 +1,6 @@
 class List extends React.Component {
   constructor(){
     super()
-
     this.state = {
       word:"",
       list : [],
@@ -11,46 +10,52 @@ class List extends React.Component {
 
   addItem(){
     if (this.state.word.length <=1) {
-        // alert("items must be more than 1 character");
         let currentMessage = "items must be more than 1 character";
         this.setState({userMessage:currentMessage});
     } else if (this.state.word.length >15) {
-        // alert("items cannot be more than 15 characters");
         let currentMessage = "items cannot be more than 15 characters";
         this.setState({userMessage:currentMessage});
     } else {
-        this.state.list.push(this.state.word);
+        let currentWord = this.state.word;
+        let currentDate = moment().format('DD MM YYYY, h:mm a')
         let currentList = this.state.list;
-        this.setState({list:currentList});
         let currentMessage = "item successfully added";
+        currentList.push({
+            word:currentWord,
+            date:currentDate
+        });
+        this.setState({list:currentList});
         this.setState({userMessage:currentMessage});
     }
   }
 
   removeItem(index){
-    this.state.list.splice(index, 1);
     let currentList = this.state.list;
+    currentList.splice(index, 1);
     this.setState({list:currentList});
   }
 
   changeHandler(){
     console.log("Typed: "+event.target.value); //broswer typed value
     let currentWord = event.target.value; //pass the typed value into currentWord
-    this.setState({word:currentWord}); //set state on the word array
+    this.setState({word:currentWord}); //set state on the word array to show on broswer
   }
 
   render() {
-      // render the list with a map() here
-      let itemsElements = this.state.list.map((item,index) => {
+    // render the list with a map() here
+    let itemsElements = this.state.list.map((item,index) => {
         return (
-            <div>
+            <div className="itemCard">
                 <ul>
-                    <li key={index+"1"}>{item}</li>
-                    <button key={index} onClick={()=>{this.removeItem(index)}} >remove item</button>
+                    <li>
+                        <p key={index+1}>{item.word}</p>
+                        <p>Posted on: {item.date}</p>
+                        <button key={index} onClick={()=>{this.removeItem(index)}}>remove item</button>
+                    </li>
                 </ul>
             </div>
         );
-      });
+    });
 
       console.log("rendering");
       return (
